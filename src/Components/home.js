4f3e9441362ed654027db89eff7dd9e5113acbf0
kevinneyer <kevinneyer@gmail.com>
2021-01-08 15:03:54 -0500

    imported bootstrap and created nominations component

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,10 +1,13 @@
-import React,  {useState, useEffect }from 'react'
+import React,  { useState }from 'react'
 import Movies from './movies'
+import Nominations from './nominations'
+import { Form, Button, Container, Row, Col }from 'react-bootstrap';
 
 const Home = () => {
 
     const [ title, setTitle ] = useState('')
     const [ movies, setMovies ] = useState([])
+    const [ nominations, setNominations ] = useState([])
 
     const titleHandler = (e) => {
         setTitle(e.target.value)
@@ -17,15 +20,35 @@ const Home = () => {
         .then(movies => setMovies(movies.Search))
         setTitle('')
     }
+
+    const clearHandler = () => {
+        setMovies([])
+    }
    
     return(
         <div>
-            <form onSubmit={submitHandler}>
+            {/* <form onSubmit={submitHandler}>
                 <input onChange={titleHandler} value={title} type='text' placeholder='search movie...' />
                 <input type='submit'></input>
-            </form>
-            {movies.length > 0 ? <Movies movies={movies}/> : null }
-            
+            </form> */}
+            <Container>
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Movie Title</Form.Label>
+                    <Form.Control onChange={titleHandler} value={title} type="text" placeholder="Enter Movie Title Here..." />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+                <Button onClick={clearHandler} variant="secondary">Clear Search</Button>
+            </Form>
+            </Container>
+            <Container>
+                <Row>
+                    <Col>{movies.length > 0 ? <Movies movies={movies}/> : null }</Col>
+                    <Col>{nominations.length > 0 ? <Nominations nominations={nominations}/> : null }</Col>
+                </Row>
+            </Container>    
         </div>
     )
 }
---
+++ src/Components/nominations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nominations = (props) => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Nominations
