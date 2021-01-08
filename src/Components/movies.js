d08641638ea45a3fed1b108c9b2bce8a610f6915
kevinneyer <kevinneyer@gmail.com>
2021-01-08 16:52:49 -0500

    disable works, plus some column styling

--- src/Components/movies.js
+++ src/Components/movies.js
@@ -3,26 +3,24 @@ import { Media, Button }from 'react-bootstrap';
 
 const Movies = (props) => {
 
-    const { movies, nominateHandler, nominations } = props
+    const { movie, nominateHandler, nominations } = props
 
     return(
         <div>
-            {movies.map(movie => 
-                <Media>
-                    <img
-                        width={64}
-                        height={64}
-                        className="mr-3"
-                        src={movie.Poster}
-                        alt={movie.Title}
-                    />
-                    <Media.Body>
-                        <h5>{movie.Title}</h5>
-                        <p>{movie.Year}</p>
-                    </Media.Body>
-                    <Button onClick={() => nominateHandler(movie)}>Nominate</Button>
-                </Media>
-            )}
+            <Media>
+                <img
+                    width={64}
+                    height={64}
+                    className="mr-3"
+                    src={movie.Poster}
+                    alt={movie.Title}
+                />
+                <Media.Body>
+                    <h5>{movie.Title}</h5>
+                    <p>{movie.Year}</p>
+                </Media.Body>
+                {nominations.includes(movie) ? <Button disabled >Nominate</Button>: <Button onClick={() => nominateHandler(movie)}>Nominate</Button>}
+            </Media>
         </div>
     )
 }
--- src/Components/nominations.js
+++ src/Components/nominations.js
@@ -1,28 +1,33 @@
 import React from 'react'
-import { Media, Button }from 'react-bootstrap';
+import { Media, Button, Card }from 'react-bootstrap';
 
 const Nominations = (props) => {
 
     const { nominations, removeHandler } = props
  
     return(
-        <div>        
-            {nominations.map(nominee => 
-                <Media>
-                    <img
-                        width={64}
-                        height={64}
-                        className="mr-3"
-                        src={nominee.Poster}
-                        alt={nominee.Title}
-                    />
-                    <Media.Body>
-                        <h5>{nominee.Title}</h5>
-                        <p>{nominee.Year}</p>
-                    </Media.Body>
-                    <Button onClick={() => removeHandler(nominee)}>Remove</Button>
-                </Media>
-            )}
+        <div>
+            <h3>Nominations</h3> 
+            <Card>  
+                <Card.Body>    
+                    {nominations.map(nominee => 
+                        <Media>
+                            <img
+                                width={64}
+                                height={64}
+                                className="mr-3"
+                                src={nominee.Poster}
+                                alt={nominee.Title}
+                            />
+                            <Media.Body>
+                                <h5>{nominee.Title}</h5>
+                                <p>{nominee.Year}</p>
+                            </Media.Body>
+                            <Button onClick={() => removeHandler(nominee)}>Remove</Button>
+                        </Media>
+                    )}
+                </Card.Body> 
+            </Card>
         </div>
     )
 }
