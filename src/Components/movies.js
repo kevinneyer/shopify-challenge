65d7e46dddd88dd0f91ecac268d28e485dd0c137
kevinneyer <kevinneyer@gmail.com>
2021-01-12 22:00:14 -0500

    updated header. last before new branch

--- src/Components/movies.js
+++ src/Components/movies.js
@@ -10,13 +10,13 @@ const Movies = (props) => {
             {movie ? 
             <Media>
                 <img
-                    width={64}
-                    height={64}
+                    width={128}
+                    // height={64}
                     className="mr-3"
                     src={movie.Poster}
                     alt={movie.Title}
                 />
-                <Media.Body>
+                 <Media.Body>
                     <h5>{movie.Title}</h5>
                     <p>{movie.Year}</p>
                 </Media.Body>
