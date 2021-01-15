d63899576ebdf9ebb52d48331994e6af7e9e477b
kevinneyer <kevinneyer@gmail.com>
2021-01-15 15:16:01 -0500

    cleaned up code and dead space

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react'
 import Home from './Components/home'
-import { Jumbotron, Row, Col, Container } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 import logo  from './assets/logo.png'
-
 
 const App = () => {
   return (
     <div  className='app'>
       <Jumbotron className='jumbo'> 
-      {/* <Container> */}
-      {/* <Row>
-        <Col> */}
-        {/* <img className='logo' src={logo} alt='logo'></img> */}
         <h1 className='header'>
           <img className='logo' src={logo} alt='logo'></img>
           The Shoppies
         </h1>
-        {/* </Col>
-        </Row> */}
-        {/* </Container>  */}
       </Jumbotron>
       <Home />
     </div>
